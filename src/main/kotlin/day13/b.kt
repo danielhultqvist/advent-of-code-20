4d938583da93b564ad33ac6097c1382554192fdf
Daniel Hultqvist <daniel.hultqvist@sl.se>
2020-12-15 21:05:38 +0100

    Solution for day13

--- src/main/kotlin/day13/b.kt
+++ src/main/kotlin/day13/b.kt
@@ -2,28 +2,50 @@ package day13b
 
 import java.io.File
 
-data class BusDeparture(val busId: Long, val offset: Long)
+data class TimeAndStep(val t: Long, val step: Long)
 
+/**
+ * General idea
+ * Find t such that
+ * (t+0) % X1 == 0
+ * (t+1) % X2 == 0
+ * ...
+ * (t+N) % Xx == 0
+ *
+ * Iterate looping through t, with increasing step size everytime a match has been
+ */
 fun main(args: Array<String>) {
-    val lines = File(args[0]).readLines()
-
-    val schedule = lines[1].split(",").mapIndexedNotNull {i, busId ->
-        if (busId == "x") {
-            null
-        } else {
-            BusDeparture(busId.toLong(), i.toLong())
+    File(args[0])
+        .readLines()[1]
+        .split(",")
+        .withIndex()
+        .filter { (_, value) -> value != "x" }
+        .fold(TimeAndStep(0, 1)) { acc, bus ->
+            val busInterval = bus.value.toLong()
+            var nextT = acc.t
+            while (((nextT + bus.index) % busInterval) != 0L) {
+                nextT += acc.step
+            }
+            TimeAndStep(nextT, acc.step * busInterval)
         }
-    }
+        .let { println(it.t) }
 
-    var stepSize = schedule[0].busId
-    var t = 0L
 
-    schedule.drop(1).forEach { (busId, offset) ->
-        while (((t + offset) % busId) != 0L) {
-            t += stepSize
+
+
+    // Alternativ 2, more readable
+    val buses = File(args[0]).readLines()[1].split(",")
+
+    var stepSize = buses[0].toLong()
+    var t = stepSize
+    buses.withIndex()
+        .drop(1)
+        .filter { (_, value) -> value != "x" }
+        .forEach { (offset, busId) ->
+            while (((t + offset) % busId.toLong()) != 0L) {
+                t += stepSize
+            }
+            stepSize *= busId.toLong()
         }
-        stepSize *= busId
-    }
-
     println(t)
 }
